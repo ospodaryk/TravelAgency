924dfe532d1b83bec1234b6c67dd24df279983ff
ospodaryk <ospodaryk11@gmail.com>
2023-06-05 15:28:55 +0300

    Added simple controller which show hotels

--- src/main/java/org/project/controllers/HotelController.java
+++ src/main/java/org/project/controllers/HotelController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -18,7 +17,7 @@ public class HotelController {
 
     @GetMapping("/hotels")
     public String showAllHotels(Model model) {
-        List<Hotel> hotels = hotelService.findAll();
+        List<Hotel> hotels = hotelService.getAllHotels();
         System.out.println("----------------------------------------------------------------");
         System.out.println(hotels);
         System.out.println("----------------------------------------------------------------");
