3183d45223e06984563d3cc1a416acc13146ea47
ospodaryk <ospodaryk11@gmail.com>
2023-06-08 18:15:10 +0300

    Added exceptions

---
+++ src/main/java/org/project/exception/EntitiyNotCreatedException.java
@@ -0,0 +1,7 @@
+package org.project.exception;
+
+public class EntitiyNotCreatedException extends RuntimeException{
+    public EntitiyNotCreatedException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/org/project/exception/ErrorResponse.java
@@ -0,0 +1,27 @@
+package org.project.exception;
+
+public class ErrorResponse {
+    private String message;
+    private Long timestamp;
+
+    public ErrorResponse(String message, Long timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
