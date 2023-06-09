c85972beef3f901070ac96319838dfacf3d1e463
ospodaryk <ospodaryk11@gmail.com>
2023-06-09 20:09:17 +0300

    Added clever delete to Booking

--- src/main/java/org/project/dao/implementation/BookingDAOImpl.java
+++ src/main/java/org/project/dao/implementation/BookingDAOImpl.java
@@ -47,7 +47,7 @@ public class BookingDAOImpl extends GenericDAOImpl<Booking, Long> implements Boo
 
     @Override
     public void delete(Booking entity) {
-        getSession().delete(entity);
+//        getSession().delete(entity);
         entity.setActual(false);
     }
 }
