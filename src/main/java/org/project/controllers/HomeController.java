cb8184c12cafee194e9c581da2b4554e9aa1d948
ospodaryk <ospodaryk11@gmail.com>
2023-06-16 12:10:31 +0300

    Updated controllers

--- src/main/java/org/project/controllers/HomeController.java
+++ src/main/java/org/project/controllers/HomeController.java
@@ -6,20 +6,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
     @GetMapping("/home")
     public String home(Model model) {
+        logger.info("Entering home");
+        logger.info("Exiting home");
         return "home";
     }
 
     @PostMapping("/home")
     public String homePage(Model model) {
+        logger.info("Entering homePage");
+        logger.info("Exiting homePage");
         return "redirect:/form-login?logout=true";
     }
 }
--- src/main/java/org/project/controllers/SecurityController.java
+++ src/main/java/org/project/controllers/SecurityController.java
@@ -12,31 +12,51 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class SecurityController {
+
+    private final Logger logger = LoggerFactory.getLogger(SecurityController.class);
 
     @Autowired
     private AuthenticationManager authenticationManager;
 
     @GetMapping({"/", "/form-login"})
     public String login(Model model, @RequestParam(value = "error", required = false) String error) {
+        logger.info("Entering login");
         if (error != null) {
             model.addAttribute("error", true);
         }
+        logger.info("Exiting login");
         return "login";
     }
 
     @PostMapping("/form-login")
     public String login(@RequestParam("username") String username, @RequestParam("password") String password, Model model) {
+        logger.info("Entering login with POST");
         try {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
             Authentication auth = authenticationManager.authenticate(token);
             SecurityContextHolder.getContext().setAuthentication(auth);
-            return "redirect:/home"; // Redirect to home page after successful login
+            logger.info("Exiting login with POST - successful login");
+            return "redirect:/home";
         } catch (AuthenticationException e) {
             model.addAttribute("error", true);
+            logger.warn("Exiting login with POST - failed login");
             return "redirect:/form-login?error=true";
         }
     }
-
 }
